b72c1ac3acadc970313862d63c0263d8bf712e81
Michael Pedersen <steele.pedersen@gmail.com>
2016-04-24 17:14:31 -0400

    Rewrite with Python3 and the Requests library

--- login.py
+++ login.py
@@ -1,116 +1,127 @@
-#!/usr/bin/python2
-#sadly mechanize isn't ported to python 3..
-#import urllib2.request
-import mechanize
+#!/usr/bin/python3
 import re
 import requests
 import os
 from bs4 import BeautifulSoup
+import json
+import youtube_dl
 
-#~ def downloadFile(url, outputPath):
-	#~ r = requests.get(url)
-	#~ with open(outputPath, "wb") as code:
-		#~ code.write(r.content.encode('utf-8'))
+moodleBaseURL = "https://moodle.uncc.edu/"
+moodleLoginURL = "https://moodle.uncc.edu/login/index.php"
+moodleNavURL = "https://moodle.uncc.edu/lib/ajax/getnavbranch.php"
+moodleAssignmentURL = "https://moodle.uncc.edu/mod/assign/index.php"
+moodleResourceURL = "https://moodle.uncc.edu/course/resources.php"
 
-def dumpclean(obj):							#https://stackoverflow.com/questions/15785719/how-to-print-a-dictionary-line-by-line-in-python
-    if type(obj) == dict:
-        for k, v in obj.items():
-            if hasattr(v, '__iter__'):
-                print k
-                dumpclean(v)
-            else:
-                print '%s : %s' % (k, v)
-    elif type(obj) == list:
-        for v in obj:
-            if hasattr(v, '__iter__'):
-                dumpclean(v)
-            else:
-                print v
-    else:
-        print obj
+# moodleBaseURL = "https://moodle2.uncc.edu/"
+# moodleLoginURL = "https://moodle2.uncc.edu/login/index.php"
+# moodleNavURL = "https://moodle2.uncc.edu/lib/ajax/getnavbranch.php"
+# moodleAssignmentURL = "https://moodle2.uncc.edu/mod/assign/index.php"
+# moodleResourceURL = "https://moodle2.uncc.edu/course/resources.php"
+baseSavePath = os.curdir
 
-moodleLoginURL = "https://moodle2.uncc.edu/login/index.php"
+s = requests.Session()
+loginPayload = {'username': 'Your Username', 'password': 'Your Password'}
+loginRequest = s.post(moodleLoginURL, data=loginPayload)
 
-br = mechanize.Browser()
-page = br.open(moodleLoginURL)
-#Get the form used by normal user to logon
-br.select_form(nr=1)
-#send login information
-br.form.set_all_readonly(False)			#Allow all forms(really everything) to be written to
-br.set_handle_robots(False)
-br.form["username"] = raw_input("Enter your Moodle 2 Username: ");
-#Select the form you want to use.
-br.select_form(nr=1)
-br.form["password"] = raw_input("Enter your Moodle 2 Password: ");
-#submit form
-response = br.submit()
+#Real request: {'elementid': 'expandable_branch_0_mycourses', 'type': '0', 'instance': '5', 'id': 'mycourses', 'sesskey': ''}
+navPayload = {
+    'elementid':'expandable_branch_0_mycourses',
+    'id':'mycourses',
+    'type':'0',
+    'sesskey':'',   #Not needed apparently...
+    'instance':'5',
+}
 
-html = response.read()
+semesterRequest = s.post(moodleNavURL, data=navPayload)
+semesterJson = json.loads(semesterRequest.text)     #Read in json as python object
 
-soup = BeautifulSoup(html)
-soup.prettify()
+semesters = []
+for semester in semesterJson['children']:
+    print("Processing semester:", semester['name'])
 
-courses = {}
-for link in soup.find_all("a"):
-	#Identify course links
-	if re.search("course/view.php\?id=\d+",link.get('href')):
-	#get linkText, year, semester, and course subject, section number and course code for each link, use it as the course's key in the dictionary
-		linkText = link.get_text()
-		year = re.search("20\d{2}",linkText)
-		year = year.group() if year else -1
-		course = re.search("([A-Z]{4})-?(\d{3,}L?)", linkText)
-		courseNum = course.groups()[1] if course else -1
-		courseSub = course.groups()[0] if course else -1
-		semester = re.search("(Fall)|(Spring)", linkText)
-		semester = semester.group() if semester else "Unknown"
-		sectionNum = re.search("-(H?\d{2,3})-",linkText)
-		sectionNum = sectionNum.groups()[0] if sectionNum  else -1
-		key = (linkText, courseSub, courseNum, sectionNum, semester, year)
-		#print key
-		courses[key] = [link.get('href')]
+    #Real Request: {'haschildren': True, 'type': 11, 'key': '34', 'title': '', 'hidden': False, 'expandable': '34', 'icon': {'component': 'moodle', 'title': '', 'alt': '', 'pix': 'i/navigationitem', 'classes': ['smallicon', 'navicon']}, 'id': 'expandable_branch_11_34', 'name': 'Spring 2016', 'class': 'type_unknown expandable_branch_11_34'}
+    coursesPayload = {
+        'elementid':semester['id'],
+        'id':semester['key'],
+        'type':semester['type'],
+        'sesskey':'',   #Not needed apparently...
+        'instance':'5',
+    }
 
+    coursesRequest = s.post(moodleNavURL, data=coursesPayload)
+    coursesJson = json.loads(coursesRequest.text)     #Read in json as python object
 
-#dumpclean(courses)
+    for course in coursesJson['children']:
+        #{'name': 'HONR-3700-H04-Fall 2015-13111', 'link': 'https://moodle2.uncc.edu/course/view.php?id=80207', 'id': 'expandable_branch_20_80207', 'key': '80207', 'type': 20, 'icon': {'title': '', 'pix': 'i/navigationitem', 'alt': '', 'component': 'moodle', 'classes': ['smallicon', 'navicon']}, 'class': 'type_course expandable_branch_20_80207', 'title': 'HONR-3700-H04-Fall 2015-Honors College Topics', 'expandable': '80207', 'haschildren': False, 'hidden': False}
+        print("\tProcessing course:", course['name'])
+        #Go to that course's page
 
-#go to each course link and get all assignments
-for key in courses:
-	print "Downloading : " + key[0]
-	print "\t URL: " + courses[key][0]
-	br.open(courses[key][0])				#open the course page
-	print "\t Page Title: " + br.title()	#check if browser is on right page
-	html = br.response().read()
-	soup = BeautifulSoup(html)
-	soup.prettify()
-	for link in br.links():
-		print "\t\t url: " + link.url
-		isCourseResourceLink = re.compile("moodle2\.uncc\.edu/mod/resource/view\.php\?id=").search(link.url)	#Course resource (powerpoint/notes/syllabus)
-		if isCourseResourceLink:
-			print "\t\t\tisCourseResourceLink"
-			#make hierarchy of "College" - year - semester - class
-			path = "./College/" + key[5] + "/" + key[4] + "/" + key[0] + "/"
-			if not os.path.exists(path): os.makedirs(path)		#make directories if they don't exist already
-			print "\t\t\t downloading to: " + path + link.text.decode('utf-8')
-			br.open(link.url)		#open the link
-			#find the direct link to the file     ex. https://moodle2.uncc.edu/pluginfile.php/792645/mod_resource/content/1/Course%20Information-Logic%20%20Algorithms%20-Spring%202014.docx
-			#~ print br.response().read()
-			#~ br._factory.is_html = True
-			try:
-				for l in br.links():
-					isDirectLink = re.search("/mod_resource/content/", l.url)
-					if isDirectLink:
-						downloadPath = path + l.text.decode('utf-8')
-						if not os.path.exists(downloadPath):
-							br.retrieve(l.url, filename = downloadPath)		#download the file
-			except mechanize._mechanize.BrowserStateError, e:			#direct link from main moodle course page, no subpage
-				print e
-				print "\t\t\tDirect Link detected"
-				#~ print br.response().read()
-				downloadPath = path + link.text.decode('utf-8')
-				if not os.path.exists(downloadPath):
-						br.retrieve(link.url, filename = downloadPath)		#download the file
-						#warning - this file will lack filetype extension
-				#~ exit()
-			#br.back()
-		#~ br.back()
-		#if re.search(link.get_text()
+        coursePayload = {'id':course['key']}
 
+        #Get all the submitted assignments for this course
+        assignmentRequest = s.get(moodleAssignmentURL, params=coursePayload)
+        soup = BeautifulSoup(assignmentRequest.text, "html.parser")
+        soup.prettify()
+        for link in soup.find_all('a', href=re.compile(".*assign/.*")):
+            #print("Getting file:",link)# link['href'])
+            submissionRequest = s.get(link['href'])
+            #Get link associated with submitted files
+            submissionSoup = BeautifulSoup(submissionRequest.text, "html.parser")
+            submissionName = submissionSoup.find('h2').text
+            print("\t\tSubmission Name:",submissionName)
+
+            submissionLinks = submissionSoup.find_all('a', href=re.compile(".*submission_files/|mod_assign/.*"))
+            submissionLinks.append(link)
+            for fileLink in submissionLinks:
+                print("\t\t\tDownloading file:", fileLink.text)
+                savePath = baseSavePath + os.sep + semester['name'] + os.sep + course['name'] + os.sep + submissionName;
+                try:
+                    os.makedirs(savePath)
+                except FileExistsError:
+                    pass
+                r = s.get(fileLink['href'], stream = True)
+                with open(savePath + os.sep + fileLink.text, "wb") as file:
+                    for chunk in r.iter_content(chunk_size=1024):
+                        if chunk: # filter out keep-alive new chunks
+                            file.write(chunk)
+                        #file.write(r.content)
+
+        #Get all the submitted assignments for this course
+        resourceRequest = s.get(moodleResourceURL, params=coursePayload)
+        soup = BeautifulSoup(resourceRequest.text, "html.parser")
+        soup.prettify()
+        for link in soup.find_all('a', href=re.compile(".*assign/.*")):
+            #print("Getting file:",link)# link['href'])
+            submissionRequest = s.get(link['href'])
+            #Get link associated with submitted files
+            resourceSoup = BeautifulSoup(resourceRequest.text, "html.parser")
+            print("\t\tGetting Resources")
+
+            resourceLinks = resourceSoup.find_all('a', href=re.compile(".*resource/|mod/url.*"))
+            resourceLinks.append(link)
+            for fileLink in resourceLinks:
+                print("\t\t\tDownloading file:", fileLink.text)
+                savePath = baseSavePath + os.sep + semester['name'] + os.sep + course['name'] + os.sep + 'Resources'
+                try:
+                    os.makedirs(savePath)
+                except FileExistsError:
+                    pass
+
+                r = s.get(fileLink['href'])
+                youtubeSoup =  BeautifulSoup(r.text, "html.parser")
+                ydl = youtube_dl.YoutubeDL({        #https://github.com/rg3/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L121-L269
+                    'quiet': True,
+                    'ignoreerrors': True,
+                    'outtmpl': savePath + os.sep + 'Videos' + os.sep + '%(title)s.%(ext)s',
+                    'retries':2,
+                    'nooverwrites':True,
+                    'restrictfilenames':True,
+                    'no_warnings':True,
+                })
+                with ydl:
+                    for youtubeLink in youtubeSoup.find_all('a', href=re.compile("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?")):
+                        print("\t\t\t\tDownloading youtube link:", youtubeLink['href'])
+                        result = ydl.extract_info(youtubeLink['href'], download=True)
+                filename = fileLink.text.replace(' ', '_').replace('/', "")
+                with open(savePath + os.sep + filename, "wb") as file:
+                    file.write(r.content)
